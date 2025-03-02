99d2432356876c9c018e4e25d6cc2ce5b150508b
mehedi-islam <144926217+mehedi-islam@users.noreply.github.com>
2025-03-02 17:31:34 +0600

    book apoinment

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
 function Doctors(props) {
+    const { speciality } =useparams()
+    console.log(speciality)
     return (
-        <div></div>
+        <div>
+
+        </div>
     );
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import SpecialityMenu from '../components/SpecialityMenu';
 import TopDoctors from '../components/TopDoctors';
+import Banner from '../components/Banner'; 
 
 function Home(props) {
     return (
@@ -9,6 +10,7 @@ function Home(props) {
             <Header/>
             <SpecialityMenu/>
             <TopDoctors/>
+            <Banner/>
         </div>
     );
 }
